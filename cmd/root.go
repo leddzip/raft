eb0d8d598e0f6d3f30d007d7a4b8bee55d445276
Leddzip <leddzip@gmail.com>
2022-04-13 00:30:34 +0200

    feature/RAFT-13_run-only-in-raft-managed-folder-or-subfolder
    slightl changing the base setup for cobra generated application

--- cmd/root.go
+++ cmd/root.go
@@ -13,15 +13,11 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "raft",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "RAFT - A language, project and purpose agnostic runner",
+	Long: `A runner that let you centralize all your build script,
+actions and scripted task around one tool so that you don't have to deal
+with multiple build tools when dealing with project with multiple techs.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	//Run: func(cmd *cobra.Command, args []string) {},
 }
 
@@ -32,16 +28,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.raft.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
