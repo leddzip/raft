cad41eaf19761276c7babbb022c2b221359a1496
Leddzip <leddzip@gmail.com>
2022-04-24 04:44:25 +0200

    feature/RAFT-1_run-a-shell-script-from-a-yaml-file-in-the-current-folder
    continue refactoring

--- internal/script_folder/domain.go
+++ internal/script_folder/domain.go
@@ -1,28 +1,7 @@
 package script_folder
 
-import (
-	backfinder "github.com/leddzip/back-finder"
-	"os"
-)
-
-func GetAppManagedFolder(folderNameToRetrieve string, fromFolder string, backToFolder string) (string, error) {
-	filePresence, err := backfinder.FindFileBetween(folderNameToRetrieve, fromFolder, backToFolder)
-	if err != nil {
-		return "", NewUnexpectedFolderErrorf("cannot assert if the folder '%s' exist or not", folderNameToRetrieve)
-	}
-
-	if !filePresence.IsFilePresent {
-		return "", NewNotAnAppManagedFolderErrorf("no folder '%s' found in the folder hierarchy. You are not in a folder managed by this tool", folderNameToRetrieve)
-	}
-
-	fileInfo, err := os.Stat(filePresence.FilePathIfExist)
-	if err != nil {
-		return "", NewUnexpectedFolderErrorf("cannot open path '%s'", filePresence.FilePathIfExist)
-	}
-
-	if fileInfo.IsDir() {
-		return filePresence.FilePathIfExist, nil
-	} else {
-		return "", NewNotAnAppManagedFolderErrorf("path '%s' is not a folder", folderNameToRetrieve)
-	}
+type Candidate struct {
+	Path     string
+	FileName string
+	Name     string
 }
